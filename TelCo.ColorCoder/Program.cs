3693dd31f8d95a24ef8214a91b7d2b794b4815e7
haripriya-pr <prharipriya1998@gmail.com>
2025-08-12 16:45:30 +0530

    Update Program.cs

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -5,16 +5,7 @@ using System.Drawing;
 namespace TelCo.ColorCoder
 {
     class Program
-    {
-        private static Color[] colorMapMajor;
-        private static Color[] colorMapMinor;
-        
-        static Program()
-        {
-            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
-            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
-        }
-       
+    {  
         private static void Main(string[] args)
         {
            TestColorCoder.TestCases();
