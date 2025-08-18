2f44f3017f9ee757e83547d042e67dc7e9e39602
haripriya p r <prharipriya1998@gmail.com>
2025-08-18 10:11:30 +0530

    Refactored the code

--- TelCo.ColorCoder/Program.cs
+++ TelCo.ColorCoder/Program.cs
@@ -5,10 +5,28 @@ using System.Drawing;
 namespace TelCo.ColorCoder
 {
     class Program
-    {  
-        private static void Main(string[] args)
+    {
+        public static Color[] colorMapMajor; /// Array of Major colors
+        public static Color[] colorMapMinor; /// Array of minor colors
+        internal class ColorPair
         {
-           TestColorCoder.TestCases();
+            internal Color majorColor;
+            internal Color minorColor;
+            public override string ToString()
+            {
+                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
+            }
+        }
+        
+        static Program() /// Static constructor required to initialize static variable
+        {
+            colorMapMajor = new Color[] { Color.White, Color.Red, Color.Black, Color.Yellow, Color.Violet };
+            colorMapMinor = new Color[] { Color.Blue, Color.Orange, Color.Green, Color.Brown, Color.SlateGray };
+        }
+
+        private static void Main(string[] args) /// Test code for the class
+        {
+            TestColorCoder.TestsForColorCoder();
         }
     }
 }
---
+++ TelCo.ColorCoder/TestColorCoder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+using static TelCo.ColorCoder.Program;
+
+namespace TelCo.ColorCoder
+{
+    internal class TestColorCoder
+    {
+        public static void TestColorFromPairNumber(int pairNumber, ColorPair resultColorPair)
+        {
+            ColorPair colorPair = ColorConversion.GetColorFromPairNumber(pairNumber);
+            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, colorPair);
+            Debug.Assert(colorPair.majorColor == resultColorPair.majorColor);
+            Debug.Assert(colorPair.minorColor == resultColorPair.minorColor);
+        }
+
+        public static void TestPairNumberFromColor(ColorPair colorPair, int resultPairNumber)
+        {
+            int pairNumber = ColorConversion.GetPairNumberFromColor(colorPair);
+            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", colorPair, pairNumber);
+            Debug.Assert(pairNumber == resultPairNumber);
+        }
+
+        public static void TestsForColorCoder()
+        {
+            TestColorFromPairNumber(4, new ColorPair { majorColor = Color.White, minorColor = Color.Brown });
+            TestColorFromPairNumber(5, new ColorPair { majorColor = Color.White, minorColor = Color.SlateGray });
+            TestColorFromPairNumber(23, new ColorPair { majorColor = Color.Violet, minorColor = Color.Green });
+            TestPairNumberFromColor(new ColorPair { majorColor = Color.Yellow, minorColor = Color.Green }, 18);
+            TestPairNumberFromColor(new ColorPair { majorColor = Color.Red, minorColor = Color.Blue }, 6);
+        }
+    }
+}
